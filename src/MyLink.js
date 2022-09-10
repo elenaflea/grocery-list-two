545261d1a2d5cc51fd091d6222b92496b5b2a982
Elena GRONDIN <88861549+elenaflea@users.noreply.github.com>
2022-09-10 20:11:19 +0200

    Update MyLink.js

--- src/MyLink.js
+++ src/MyLink.js
@@ -2,6 +2,7 @@ function MyLinks () {
 
     return(
         <div className="footer">
+        <br />
             <a className='links' href='https://lunacode.fr/' target="_blank" rel="noreferrer">
                 Developed with 🧡 by Elena GRONDIN
             </a>
